9ad90a425c591743fda0a85e08ff1329d7d866a4
shkiimaa <shkiimaa@gmail.com>
2023-07-13 00:44:31 +0900

    [Style] modify borrow position table

--- src/components/BorrowPositionTable/TokenTable.tsx
+++ src/components/BorrowPositionTable/TokenTable.tsx
@@ -1,32 +1,32 @@
 import { Trans } from '@lingui/macro'
-import { PAGE_SIZE, useTopTokens } from 'graphql/data/TopTokens'
+import { PAGE_SIZE } from 'graphql/data/TopTokens'
 import { validateUrlChainParam } from 'graphql/data/util'
 import { ReactNode } from 'react'
-import { AlertTriangle } from 'react-feather'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import { LimitlessPositionDetails } from 'types/leveragePosition'
 
 import { MAX_WIDTH_MEDIA_BREAKPOINT } from './constants'
 import { HeaderRow, LoadedRow, LoadingRow } from './TokenRow'
-import { LimitlessPositionDetails } from 'types/leveragePosition'
-import { ThemedText } from 'theme'
 
 const GridContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width:100%;
-  max-width: ${MAX_WIDTH_MEDIA_BREAKPOINT};
+  // margin-top: 8px;
+  width: 100%;
+  /* max-width: ${MAX_WIDTH_MEDIA_BREAKPOINT}; */
   background-color: ${({ theme }) => theme.backgroundSurface};
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
-    0px 24px 32px rgba(0, 0, 0, 0.01);
-  margin-left: auto;
-  margin-right: auto;
+  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04),
+    0px 16px 24px rgba(0, 0, 0, 0.04), 0px 24px 32px rgba(0, 0, 0, 0.01);
+  /* margin-left: auto;
+  margin-right: auto; */
+  padding: 20px 0;
   border-radius: 12px;
   border-top-left-radius: 0;
   justify-content: center;
   align-items: center;
   // border: 1px solid ${({ theme }) => theme.backgroundOutline};
-`
+`;
 
 const TokenDataContainer = styled.div`
   display: flex;
@@ -94,7 +94,7 @@ export default function PositionsTable({positions, loading}: {positions?: Limitl
         <HeaderRow />
         <TokenDataContainer>
           {positions.map(
-            (position, index) =>
+            (position) =>
             position?.tokenId && (
                 <LoadedRow
                   key={position.tokenId}
