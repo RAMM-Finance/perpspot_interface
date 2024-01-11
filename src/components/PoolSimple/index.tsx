8a8ad18d83ddd3345f9efb5ce641434ccc9a4573
jeong <hpark2017@gmail.com>
2024-01-12 01:00:08 +0900

    updates

--- src/components/PoolSimple/index.tsx
+++ src/components/PoolSimple/index.tsx
@@ -318,6 +318,10 @@ export default function SimplePool() {
         const rawData = await vaultContract.getData()
         setData(rawData)
 
+        const maxWithdrawableWETH = await vaultContract.maxRedeemableInToken("0x82aF49447D8a07e3bd95BD0d56f35241523fBab1")
+        const maxWithdrawableWBTC = await vaultContract.maxRedeemableInToken("0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f")
+        const maxWithdrawableUSDC = await vaultContract.maxRedeemableInToken("0xaf88d065e77c8cC2239327C5EDb3A432268e5831")
+
         // rawdata[0] is total supply 
         // 1 is totalbacking 
         // 2 is utilization rate, i.e 0.5*1e18 is 50% 
