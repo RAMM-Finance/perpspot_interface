7c9da77fd45e533eaa6e2c240a6e8c7fcdf563dd
vpat95 <89660793+vpat95@users.noreply.github.com>
2024-05-16 21:06:55 -0400

    updates

--- src/components/Launch/CreateCoinModal.tsx
+++ src/components/Launch/CreateCoinModal.tsx
@@ -1,73 +1,8 @@
 import { SmallButtonPrimary } from 'components/Button'
-import { LightCard } from 'components/Card'
-import Column from 'components/Column'
 import Modal from 'components/Modal'
 import { useState } from 'react'
 import styled from 'styled-components/macro'
 import { ThemedText } from 'theme'
-
-const ModalInfoWrapper = styled(LightCard)`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  border: none;
-  border-radius: 0px;
-  width: 100%;
-  border-bottom: 3px solid ${({ theme }) => theme.searchOutline};
-  justify-content: flex-start;
-  background: ${({ theme }) => theme.backgroundSurface};
-  padding: 0.75rem;
-`
-
-const ModalActionButton = styled(ThemedText.BodySecondary)`
-  width: 45%;
-  height: 35px;
-  display: flex;
-  align-items: center;
-  padding: 8px 0px;
-  color: ${({ theme }) => theme.accentTextLightPrimary};
-  background: ${({ theme }) => theme.accentAction};
-  transition: ${({ theme }) =>
-    `${theme.transition.duration.medium} ${theme.transition.timing.ease} bottom, ${theme.transition.duration.medium} ${theme.transition.timing.ease} visibility`};
-  will-change: transform;
-  border-radius: 8px;
-  justify-content: center;
-  white-space: nowrap;
-  font-weight: 600 !important;
-  font-size: 18px !important;
-  line-height: 16px;
-
-  &:before {
-    background-size: 100%;
-    border-radius: inherit;
-
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 100%;
-    content: '';
-  }
-`
-
-const StyledMediaImg = styled.img<{
-  $aspectRatio?: string
-  $hidden?: boolean
-}>`
-  width: 40%;
-  height: 50%;
-  /* aspect-ratio: ${({ $aspectRatio }) => $aspectRatio}; */
-  aspect-ratio: 16/9;
-  transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} transform`};
-  will-change: transform;
-  object-fit: contain;
-  border-radius: 12px;
-
-  @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.lg}px`}) {
-    width: 70%;
-  }
-`
 
 const ModalWrapper = styled.div`
   display: flex;
@@ -79,22 +14,8 @@ const ModalWrapper = styled.div`
   border-radius: 20px;
 `
 
-const ModalTopWrapper = styled.div`
-  display: flex;
-  gap: 2rem;
-  @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.lg}px`}) {
-    flex-wrap: wrap;
-  }
-`
-
-const ModalDescriptWrapper = styled(Column)<{ marginTop?: number }>`
-  margin-top: ${({ marginTop }) => marginTop && `${marginTop}px`};
-  padding: 0.5rem;
-  width: 100%;
-  align-self: start;
-`
 const ModalInputWrapper = styled.div`
-  width: 90%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 10px;
@@ -113,6 +34,7 @@ const Input = styled.input<{ large?: boolean }>`
   }
   color: white;
   padding-left: 5px;
+  width: 400px;
 `
 const InputContainer = styled.div`
   display: flex;
@@ -133,8 +55,34 @@ const CreateCoinModal = ({ isOpen, handleCloseModal }: IBoxModalProps) => {
     name: string
     ticker: string
     description: string
+    image: FileList | undefined | null
   }
-  const [coinDetails, setCoinDetails] = useState<CoinDetails>({ name: '', ticker: '', description: '' })
+  const [coinDetails, setCoinDetails] = useState<CoinDetails>({
+    name: '',
+    ticker: '',
+    description: '',
+    image: null,
+  })
+
+  const [coinData, setCoinData] = useState<CoinDetails>({
+    name: '',
+    ticker: '',
+    description: '',
+    image: null,
+  })
+
+  function handleSubmit(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault()
+    setCoinData({
+      ...coinData,
+      name: coinDetails.name,
+      ticker: coinDetails.ticker,
+      description: coinDetails.description,
+      image: coinDetails.image,
+    })
+  }
+
+  console.log('coindata', coinData)
   return (
     <Modal
       isOpen={isOpen}
@@ -150,24 +98,30 @@ const CreateCoinModal = ({ isOpen, handleCloseModal }: IBoxModalProps) => {
           <ThemedText.DeprecatedMediumHeader>Coin Details</ThemedText.DeprecatedMediumHeader>
         </ModalHeader>
         <ModalInputWrapper>
-          <InputContainer>
-            <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Name</ThemedText.BodyPrimary>
-            <Input onChange={(e) => setCoinDetails({ ...coinDetails, name: e.target.value })} />
-          </InputContainer>
-          <InputContainer>
-            <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Ticker</ThemedText.BodyPrimary>
-            <Input onChange={(e) => setCoinDetails({ ...coinDetails, ticker: e.target.value })} />
-          </InputContainer>
-          <InputContainer>
-            <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Description</ThemedText.BodyPrimary>
-            <Input onChange={(e) => setCoinDetails({ ...coinDetails, description: e.target.value })} large={true} />
-          </InputContainer>
-          <InputContainer>
-            <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Image</ThemedText.BodyPrimary>
-            <Input type="file" />
-          </InputContainer>
-          <SmallButtonPrimary>Create Coin</SmallButtonPrimary>
-          <ThemedText.BodySmall>Cost to deploy:</ThemedText.BodySmall>
+          <form id="form">
+            <InputContainer>
+              <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Name</ThemedText.BodyPrimary>
+              <Input onChange={(e) => setCoinDetails({ ...coinDetails, name: e.target.value })} />
+            </InputContainer>
+            <InputContainer>
+              <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Ticker</ThemedText.BodyPrimary>
+              <Input onChange={(e) => setCoinDetails({ ...coinDetails, ticker: e.target.value })} />
+            </InputContainer>
+            <InputContainer>
+              <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Description</ThemedText.BodyPrimary>
+              <Input onChange={(e) => setCoinDetails({ ...coinDetails, description: e.target.value })} large={true} />
+            </InputContainer>
+            <InputContainer>
+              <ThemedText.BodyPrimary style={{ marginLeft: '10px' }}>Image</ThemedText.BodyPrimary>
+              <Input type="file" onChange={(e) => setCoinDetails({ ...coinDetails, image: e.target.files })} />
+            </InputContainer>
+          </form>
+          <SmallButtonPrimary type="submit" onClick={(e) => handleSubmit}>
+            Create Coin
+          </SmallButtonPrimary>
+          <ThemedText.BodySmall fontSize={13} fontWeight={900}>
+            Cost to deploy:
+          </ThemedText.BodySmall>
         </ModalInputWrapper>
       </ModalWrapper>
     </Modal>
--- src/pages/Launch/index.tsx
+++ src/pages/Launch/index.tsx
@@ -1,8 +1,11 @@
 import Column from 'components/Column'
 import CreateCoinModal from 'components/Launch/CreateCoinModal'
+import Row from 'components/Row'
 import React, { useCallback, useState } from 'react'
 import styled from 'styled-components/macro'
+import { BREAKPOINTS, ThemedText } from 'theme'
 
+import ItemImg from '../../assets/images/newItem7.webp'
 import banner from '../../components/Launch/launchBanner.png'
 
 const BannerWrapper = styled.div`
@@ -103,12 +106,58 @@ const Input = styled.input`
   color: white;
   padding-left: 5px;
 `
-const TableWrapper = styled.div`
+const TableWrapper = styled.div<{ modalOpen: boolean }>`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   margin-left: 150px;
   margin-right: 50px;
   margin-top: 20px;
+  z-index: ${({ modalOpen }) => (modalOpen ? '10' : '1100')};
 `
+const LeadCoinWrapper = styled.div`
+  width: 85%;
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  align-items: center;
+  gap: 20px;
+`
+const CoinListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 30px;
+  width: 100%;
+  margin-top: 30px;
+  margin-right: 10px;
+`
+
+const CardDetailsWrapper = styled.div`
+  position: relative;
+  display: flex;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.backgroundSurface};
+  overflow: hidden;
+  box-shadow: 0px 0px 8px rgba(51, 53, 72, 0.04), 1px 2px 4px rgba(51, 53, 72, 0.12);
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  isolation: isolate;
+  max-width: 400px;
+`
+const DetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 10px;
+  margin-left: 10px;
+`
+const DetailSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`
+
+const CoinBox = styled.div``
 
 export const Launch = () => {
   const [showModal, setShowModal] = useState(false)
@@ -132,9 +181,354 @@ export const Launch = () => {
           </BannerTextWrapper>
         </BannerInfoWrapper>
       </CollectionContainer>
-      <TableWrapper>
+      <TableWrapper modalOpen={showModal}>
+        <LeadCoinWrapper>
+          <ThemedText.DeprecatedMediumHeader>King of the Hill</ThemedText.DeprecatedMediumHeader>
+          <StyledCardContainerLead>
+            <ItemImgContainer>
+              <StyledMediaContainer>
+                <StyledMediaImg src={ItemImg} />
+              </StyledMediaContainer>
+            </ItemImgContainer>
+            <StyledDetailsRelativeContainer>
+              <StyledDetailsContainer>
+                <StyledInfoContainer>
+                  <Row gap="8" justifyContent="space-between">
+                    <StyledPrimaryDetails>
+                      <PrimaryInfoContainer>Token A </PrimaryInfoContainer>
+                    </StyledPrimaryDetails>
+                  </Row>
+                  <Row justifyContent="space-between"></Row>
+                </StyledInfoContainer>
+              </StyledDetailsContainer>
+            </StyledDetailsRelativeContainer>
+            <StyledActionButton> More Details</StyledActionButton>
+          </StyledCardContainerLead>
+        </LeadCoinWrapper>
         <Input placeholder="Search Coins" />
+        <CoinListWrapper>
+          <CardDetailsWrapper>
+            <StyledCardContainer>
+              <ItemImgContainer>
+                <StyledMediaContainer>
+                  <StyledMediaImg src={ItemImg} />
+                </StyledMediaContainer>
+              </ItemImgContainer>
+              <StyledDetailsRelativeContainer>
+                <StyledDetailsContainer>
+                  <StyledInfoContainer>
+                    <Row gap="8" justifyContent="space-between">
+                      <StyledPrimaryDetails>
+                        <PrimaryInfoContainer>Token B </PrimaryInfoContainer>
+                      </StyledPrimaryDetails>
+                    </Row>
+                    <Row justifyContent="space-between"></Row>
+                  </StyledInfoContainer>
+                </StyledDetailsContainer>
+              </StyledDetailsRelativeContainer>
+              <StyledActionButton> More Details</StyledActionButton>
+            </StyledCardContainer>
+            <DetailsWrapper>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Created By:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">0x2312310123243143</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Market Cap:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">12.6k</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Replies:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">375</ThemedText.BodySmall>
+              </DetailSection>
+            </DetailsWrapper>
+          </CardDetailsWrapper>
+          <CardDetailsWrapper>
+            <StyledCardContainer>
+              <ItemImgContainer>
+                <StyledMediaContainer>
+                  <StyledMediaImg src={ItemImg} />
+                </StyledMediaContainer>
+              </ItemImgContainer>
+              <StyledDetailsRelativeContainer>
+                <StyledDetailsContainer>
+                  <StyledInfoContainer>
+                    <Row gap="8" justifyContent="space-between">
+                      <StyledPrimaryDetails>
+                        <PrimaryInfoContainer>Token C </PrimaryInfoContainer>
+                      </StyledPrimaryDetails>
+                    </Row>
+                    <Row justifyContent="space-between"></Row>
+                  </StyledInfoContainer>
+                </StyledDetailsContainer>
+              </StyledDetailsRelativeContainer>
+              <StyledActionButton> More Details</StyledActionButton>
+            </StyledCardContainer>
+            <DetailsWrapper>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Created By:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">0x2312310123243143</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Market Cap:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">12.6k</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Replies:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">375</ThemedText.BodySmall>
+              </DetailSection>
+            </DetailsWrapper>
+          </CardDetailsWrapper>
+          <CardDetailsWrapper>
+            <StyledCardContainer>
+              <ItemImgContainer>
+                <StyledMediaContainer>
+                  <StyledMediaImg src={ItemImg} />
+                </StyledMediaContainer>
+              </ItemImgContainer>
+              <StyledDetailsRelativeContainer>
+                <StyledDetailsContainer>
+                  <StyledInfoContainer>
+                    <Row gap="8" justifyContent="space-between">
+                      <StyledPrimaryDetails>
+                        <PrimaryInfoContainer>Token D </PrimaryInfoContainer>
+                      </StyledPrimaryDetails>
+                    </Row>
+                    <Row justifyContent="space-between"></Row>
+                  </StyledInfoContainer>
+                </StyledDetailsContainer>
+              </StyledDetailsRelativeContainer>
+              <StyledActionButton> More Details</StyledActionButton>
+            </StyledCardContainer>
+            <DetailsWrapper>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Created By:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">0x2312310123243143</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Market Cap:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">12.6k</ThemedText.BodySmall>
+              </DetailSection>
+              <DetailSection>
+                <ThemedText.BodySmall fontWeight={900}>Replies:</ThemedText.BodySmall>
+                <ThemedText.BodySmall color="textSecondary">375</ThemedText.BodySmall>
+              </DetailSection>
+            </DetailsWrapper>
+          </CardDetailsWrapper>
+        </CoinListWrapper>
       </TableWrapper>
     </>
   )
 }
+
+const ItemImgContainer = styled.div`
+  position: relative;
+  pointer-events: auto;
+  &:hover {
+    opacity: theme.opacity.enabled;
+  }
+  cursor: pointer;
+`
+
+const StyledMediaContainer = styled(Row)`
+  overflow: hidden;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+`
+
+const StyledMediaImg = styled.img<{
+  // imageLoading: boolean
+  $aspectRatio?: string
+  $hidden?: boolean
+}>`
+  width: 100%;
+  /* aspect-ratio: ${({ $aspectRatio }) => $aspectRatio}; */
+  aspect-ratio: 16/9;
+  transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} transform`};
+  will-change: transform;
+  object-fit: contain;
+  visibility: ${({ $hidden }) => ($hidden ? 'hidden' : 'visible')};
+`
+
+const StyledDetailsRelativeContainer = styled.div`
+  position: relative;
+  height: 50px;
+`
+
+const StyledDetailsContainer = styled(Column)`
+  position: absolute;
+  width: 100%;
+  padding: 16px 8px 0px;
+  justify-content: space-between;
+  gap: 8px;
+  height: 84px;
+  background: ${({ theme }) => theme.backgroundSurface};
+  will-change: transform;
+  transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} transform`};
+
+  @media screen and (max-width: ${BREAKPOINTS.sm}px) {
+    height: 112px;
+    transform: translateY(-28px);
+  }
+`
+
+const StyledInfoContainer = styled(Column)`
+  gap: 4px;
+  overflow: hidden;
+  width: 100%;
+  padding: 0px 8px;
+  height: 48px;
+`
+
+const StyledPrimaryDetails = styled(Row)`
+  width: 100%;
+  justify-content: space-between;
+  /* justify-items: center; */
+  overflow: hidden;
+  white-space: nowrap;
+  gap: 8px;
+`
+
+const PrimaryInfoContainer = styled(ThemedText.BodyPrimary)`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  font-weight: 600 !important;
+  line-height: 20px;
+`
+
+const StyledActionButton = styled(ThemedText.BodySecondary)`
+  position: absolute;
+  display: flex;
+  padding: 8px 0px;
+  bottom: -32px;
+  left: 8px;
+  right: 8px;
+  color: ${({ theme }) => theme.accentTextLightPrimary};
+  background: ${({ theme }) => theme.accentAction};
+  transition: ${({ theme }) =>
+    `${theme.transition.duration.medium} ${theme.transition.timing.ease} bottom, ${theme.transition.duration.medium} ${theme.transition.timing.ease} visibility`};
+  will-change: transform;
+  border-radius: 8px;
+  justify-content: center;
+  font-weight: 600 !important;
+  line-height: 16px;
+  visibility: hidden;
+  cursor: pointer;
+
+  @media screen and (max-width: ${BREAKPOINTS.sm}px) {
+    visibility: visible;
+    bottom: 8px;
+  }
+
+  &:before {
+    background-size: 100%;
+    border-radius: inherit;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+    content: '';
+  }
+
+  &:hover:before {
+    background-color: ${({ theme }) => theme.stateOverlayHover};
+  }
+
+  &:active:before {
+    background-color: ${({ theme }) => theme.stateOverlayPressed};
+  }
+`
+
+const StyledCardContainerLead = styled.div`
+  position: relative;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.backgroundSurface};
+  overflow: hidden;
+  box-shadow: 0px 0px 8px rgba(51, 53, 72, 0.04), 1px 2px 4px rgba(51, 53, 72, 0.12);
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  isolation: isolate;
+  max-width: 150px;
+
+  -webkit-box-shadow: 0 0 20px blue;
+  -moz-box-shadow: 0 0 20px blue;
+  box-shadow: 0 0 20px blue;
+
+  :after {
+    content: '';
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    border: 1px solid;
+    border-radius: 12px;
+    border-color: ${({ theme }) => theme.backgroundOutline};
+    pointer-events: none;
+    transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} border`};
+    will-change: border;
+  }
+
+  :hover {
+    ${StyledActionButton} {
+      visibility: visible;
+      bottom: 8px;
+    }
+
+    ${StyledDetailsContainer} {
+      height: 112px;
+      transform: translateY(-28px);
+    }
+    ${StyledMediaImg} {
+      transform: scale(1.15);
+    }
+  }
+`
+
+const StyledCardContainer = styled.div`
+  position: relative;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.backgroundSurface};
+  overflow: hidden;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  isolation: isolate;
+  max-width: 250px;
+
+  :after {
+    content: '';
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    border-top: 1px solid;
+    border-left: 1px solid;
+    border-bottom: 1px solid;
+
+    border-radius: 12px;
+    border-color: ${({ theme }) => theme.backgroundOutline};
+    pointer-events: none;
+    transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} border`};
+    will-change: border;
+  }
+
+  :hover {
+    ${StyledActionButton} {
+      visibility: visible;
+      bottom: 8px;
+    }
+
+    ${StyledDetailsContainer} {
+      height: 112px;
+      transform: translateY(-28px);
+    }
+    ${StyledMediaImg} {
+      transform: scale(1.15);
+    }
+  }
+`
