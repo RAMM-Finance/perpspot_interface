b2d5a27d17b206b08ced81e4d95a32c7a585fede
jihyun song <jihyunsong42@gmail.com>
2024-05-31 13:08:21 +0900

    added numberOfTrade chart

--- src/components/Charts/TradeChart.tsx
+++ src/components/Charts/TradeChart.tsx
@@ -16,22 +16,22 @@ const ChartContainer = styled.div`
 
 const data: any[] = [];
 
-const VolumeChart = (props: any) => {
+const TradeChart = (props: any) => {
 
   let volumeLine = 0
   const formattedData = props?.volumeByDay?.map((item: any) => {
     const date = new Date(item.timestamp * 1000);
     const formattedDate = date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
-    volumeLine += item.volume;
+    volumeLine += item.count;
     return { 
       timestamp: formattedDate, 
-      volume: item.volume, 
+      count: item.count, 
       line: volumeLine }
   })
   
   return (
     <ChartContainer>
-      <ChartHeader>Volume</ChartHeader>
+      <ChartHeader>NUMBER OF TRADES</ChartHeader>
       <ResponsiveContainer width="100%" height={400}>
         <ComposedChart
           data={formattedData}
@@ -46,7 +46,7 @@ const VolumeChart = (props: any) => {
           <YAxis tick={{ fontSize: 12 }} orientation="right" yAxisId="right" />
           <Tooltip />
           <Legend />
-          <Bar type="monotone" dataKey="volume" barSize={16} stackId="a" fill="#413ea0" name="VOLUME" />
+          <Bar type="monotone" dataKey="count" barSize={16} stackId="a" fill="#413ea0" name="NUMBER OF TRADES" />
           <Line type="monotone" dataKey="line" stroke="#ff7300" strokeWidth={3} dot={false} yAxisId="right" name="Cumulative" />
         </ComposedChart>
       </ResponsiveContainer>
@@ -54,4 +54,4 @@ const VolumeChart = (props: any) => {
   )
 }
 
-export default VolumeChart
+export default TradeChart
