b14d63242eaa9d129d2385affe189a9a7e4cf211
jihyun song <jihyunsong42@gmail.com>
2024-07-02 21:51:42 +0900

    Update TokenTable.tsx

--- src/components/PositionTable/LeveragePositionTable/TokenTable.tsx
+++ src/components/PositionTable/LeveragePositionTable/TokenTable.tsx
@@ -216,7 +216,7 @@ export default function LeveragePositionsTable({
     resetFilterString()
   }, [location, resetFilterString])
   const { poolMap } = usePoolKeyList()
-  console.log("TICK IN TOKEN TABLE", poolMap?.['0x4200000000000000000000000000000000000006-0x833589fcd6edb6e08f4c7c32d4f71b54bda02913-500'].tick)
+  // console.log("TICK IN TOKEN TABLE", poolMap?.['0x4200000000000000000000000000000000000006-0x833589fcd6edb6e08f4c7c32d4f71b54bda02913-500'].tick)
   const { pools: poolPrices, tokens, loading: priceLoading } = useAllPoolAndTokenPriceData()
   const currentPool = useCurrentPool()
   const setCurrentPool = useSetCurrentPool()
