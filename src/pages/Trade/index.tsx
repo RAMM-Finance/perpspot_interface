43fd8db428927f3713877b36b939ef5910443f47
jihyun song <jihyunsong42@gmail.com>
2024-07-10 11:10:49 +0900

    Update index.tsx

--- src/pages/Trade/index.tsx
+++ src/pages/Trade/index.tsx
@@ -303,8 +303,6 @@ export default function Trade({ className }: { className?: string }) {
   const token1 = useCurrency(poolKey?.token1)
   const [, pool] = usePoolV2(token0 ?? undefined, token1 ?? undefined, poolKey?.fee ?? undefined)
 
-  console.log("USE POOL TEST RES", pool?.token0.symbol, pool?.token1.symbol)
-
   const swapIsUnsupported = useIsSwapUnsupported(inputCurrency, outputCurrency)
 
   const { loading: leverageLoading, positions: leveragePositions } = useLeveragedLMTPositions(account)
@@ -384,8 +382,6 @@ export default function Trade({ className }: { className?: string }) {
       fee: pool.fee,
     })
   }, [pool])
-
-  console.log("TEST", chartSymbol, match, poolOHLC?.token0IsBase)
 
   return (
     <Trace page={InterfacePageName.SWAP_PAGE} shouldLogImpression>
